b89c6d7202a03fff9b2961893022e5d31daf4189
Stephen Yeargin <stephen.yeargin@gmail.com>
2023-12-25 23:02:57 -0600

    Fix loader

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-fs = require 'fs'
-path = require 'path'
+const path = require('path');
 
-module.exports = (robot, scripts) ->
-  scriptsPath = path.resolve(__dirname, 'src')
-  if fs.existsSync scriptsPath
-    for script in fs.readdirSync(scriptsPath).sort()
-      if scripts? and '*' not in scripts
-        robot.loadFile(scriptsPath, script) if script in scripts
-      else
-        robot.loadFile(scriptsPath, script)
+module.exports = (robot) => {
+  const scriptsPath = path.resolve(__dirname, 'src');
+  robot.loadFile(scriptsPath, 'nashville-news.js');
+};
